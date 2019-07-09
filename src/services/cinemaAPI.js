19cd8d205b67f7494d249d3e5231571d86f49327
ostepano <ostepano@softserveinc.com>
2019-07-09 15:30:21 +0300

    cinema api service update

--- src/services/cinemaAPI.js
+++ src/services/cinemaAPI.js
@@ -5,32 +5,47 @@
 import axios from "axios";
 
 const API_KEY = "pol1kh111";
-const BASE_URL = "https://api.kino-teatr.ua/";
+const BASE_URL = "api.kino-teatr.ua";
+const PROXY = "cors-anywhere.herokuapp.com";
 
 const getPremiersByCityId = (id) => {
-    return axios.get(`${BASE_URL}rest/city/${id}/shows?apiKey=${API_KEY}&size=1&date=2019-07-10&detalization=FULL`)
+    return axios.get(`https://${PROXY}/${BASE_URL}/rest/city/${id}/shows?apiKey=${API_KEY}&size=100&date=2019-07-10&detalization=FULL`)
 };
 
 const getCinemasByCityId = (id) => {
-    return axios.get(`${BASE_URL}rest/city/${id}/cinemas?apiKey=${API_KEY}&size=1`)
+    return axios.get(`https://${PROXY}/${BASE_URL}/rest/city/${id}/cinemas?apiKey=${API_KEY}&size=100`)
 };
 
 const getMovieById = (id) => {
-    return axios.get(`${BASE_URL}rest/film/${id}?apiKey=${API_KEY}`)
+    return axios.get(`https://${PROXY}/${BASE_URL}/rest/film/${id}?apiKey=${API_KEY}`)
 };
 
 const getCinemaById = (id) => {
-    return axios.get(`${BASE_URL}rest/city/${id}/shows?apiKey=${API_KEY}&size=1&date=2019-07-10&detalization=FULL`)
+    return axios.get(`https://${PROXY}/${BASE_URL}/rest/cinema/${id}?apiKey=${API_KEY}`)
 };
 
-const getPosterByMovieId = (id) => {
-    return axios.get(`${BASE_URL}rest/city/${id}/shows?apiKey=${API_KEY}&size=1&date=2019-07-10&detalization=FULL`)
-};
+// NO longer valid
+// To get an image, URL should be added to its src attribute
+
+// const getPosterByMovieId = (id) => {
+//     return axios.get(`https://${PROXY}/${BASE_ADDRESS}/public/film/${id}/poster?width=300&height=400&ratio=1`)
+// };
 
 const getActorsByMovieId = (id) => {
-    return axios.get(`${BASE_URL}rest/city/${id}/shows?apiKey=${API_KEY}&size=1&date=2019-07-10&detalization=FULL`)
+    return axios.get(`https://${PROXY}/${BASE_URL}/rest/film/${id}/persons?apiKey=${API_KEY}&size=100&detalization=FULL`)
 };
 
 const getSessionsByMovieIdAndDate = (id, date) => {
-    return axios.get(`${BASE_URL}rest/city/${id}/shows?apiKey=${API_KEY}&size=1&date=2019-07-10&detalization=FULL`)
+    return axios.get(`https://${PROXY}/${BASE_URL}/rest/cinema/${id}/shows?apiKey=${API_KEY}&size=100&date=${date}&detalization=FULL`)
 };
+
+const api = {
+    getPremiersByCityId,
+    getCinemasByCityId,
+    getMovieById,
+    getCinemaById,
+    getActorsByMovieId,
+    getSessionsByMovieIdAndDate
+};
+
+export default api
