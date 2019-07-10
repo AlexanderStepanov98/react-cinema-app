30b2bea46a35ff3ca5e11927ee8fd143cbc29b71
ostepano <ostepano@softserveinc.com>
2019-07-10 17:41:37 +0300

    cinema api update

--- src/services/cinemaAPI.js
+++ src/services/cinemaAPI.js
@@ -1,51 +1,81 @@
-// API service for requesting data from kino-teatr.ua
-// Each function returns a promise, which then needs to be resolved
-// And dispatched into the redux store (or rendered directly into UI ?)
-
 import axios from "axios";
 
-const API_KEY = "pol1kh111";
-const BASE_URL = "api.kino-teatr.ua";
 const PROXY = "cors-anywhere.herokuapp.com";
+const BASE_URL = `https://${PROXY}/api.kino-teatr.ua`;
+const API_KEY = "pol1kh111";
+const DEFAULT_RESPONSE_SIZE = 100;
+const KEY_AND_SIZE = `apiKey=${API_KEY}&size=${DEFAULT_RESPONSE_SIZE}`;
 
-const getPremiersByCityId = (id) => {
-    return axios.get(`https://${PROXY}/${BASE_URL}/rest/city/${id}/shows?apiKey=${API_KEY}&size=100&date=2019-07-10&detalization=FULL`)
+const getCities = () => {
+    return axios.get(`${BASE_URL}/rest/cities?${KEY_AND_SIZE}`)
+                .then(resp => resp.data.content)
+};
+
+const getDistributionsByCityId = (id) => {
+    return axios.get(`${BASE_URL}/rest/city/${id}/shows?${KEY_AND_SIZE}&detalization=FULL`)
+                .then(resp => resp.data.films)
 };
 
 const getCinemasByCityId = (id) => {
-    return axios.get(`https://${PROXY}/${BASE_URL}/rest/city/${id}/cinemas?apiKey=${API_KEY}&size=100`)
+    return axios.get(`${BASE_URL}/rest/city/${id}/cinemas?${KEY_AND_SIZE}`)
+                .then(resp => resp.data.content)
 };
 
 const getMovieById = (id) => {
-    return axios.get(`https://${PROXY}/${BASE_URL}/rest/film/${id}?apiKey=${API_KEY}`)
+    return axios.get(`${BASE_URL}/rest/film/${id}?apiKey=${API_KEY}`)
+                .then(resp => resp.data)
 };
 
 const getCinemaById = (id) => {
-    return axios.get(`https://${PROXY}/${BASE_URL}/rest/cinema/${id}?apiKey=${API_KEY}`)
+    return axios.get(`${BASE_URL}/rest/cinema/${id}?apiKey=${API_KEY}`)
+                .then(resp => resp.data)
 };
 
-// NO longer valid
-// To get an image, URL should be added to its src attribute
-
-// const getPosterByMovieId = (id) => {
-//     return axios.get(`https://${PROXY}/${BASE_ADDRESS}/public/film/${id}/poster?width=300&height=400&ratio=1`)
-// };
+const getPosterLinkByMovieId = (id) => {
+    return `${BASE_URL}/public/film/${id}/poster?width=300&height=400&ratio=1`
+};
 
 const getActorsByMovieId = (id) => {
-    return axios.get(`https://${PROXY}/${BASE_URL}/rest/film/${id}/persons?apiKey=${API_KEY}&size=100&detalization=FULL`)
+    return axios.get(`${BASE_URL}/rest/film/${id}/persons?${KEY_AND_SIZE}&detalization=FULL`)
+                .then(resp => resp.data.persons)
 };
 
-const getSessionsByMovieIdAndDate = (id, date) => {
-    return axios.get(`https://${PROXY}/${BASE_URL}/rest/cinema/${id}/shows?apiKey=${API_KEY}&size=100&date=${date}&detalization=FULL`)
+const getSessionsByCityIdAndMovieIdAndDate = (cityId, movieId, date) => {
+    return axios.get(`${BASE_URL}/rest/city/${cityId}/film/${movieId}/shows?${KEY_AND_SIZE}&date=${date}&detalization=FULL`)
+};
+
+const getAllSessions = (cityId, movieId, date) => {
+    return getCinemasByCityId(cityId)
+           .then(resp => Promise.all(resp.map(cinema => getSessionInCinema(cinema.id, movieId, date))))
+           .then(resp => resp.filter(item => item !== undefined))
+};
+
+// resp.data.content.forEach(item => data.push(...item.times)); - should be refactored in less complicated way
+const getSessionInCinema = (cinemaId, movieId, date) => {
+    return axios.get(`${BASE_URL}/rest/cinema/${cinemaId}/film/${movieId}/shows?${KEY_AND_SIZE}&date=${date}&detalization=FULL`)
+                .then(resp => {
+                    if (resp.data.cinemas[0] !== undefined) {
+                        const data = [];
+                        resp.data.content.forEach(item => data.push(...item.times));
+                        return {
+                            cinema: resp.data.cinemas[0].name,
+                            sessions: data
+                        }
+                    }
+                })
 };
 
 const api = {
-    getPremiersByCityId,
+    getCities,
+    getDistributionsByCityId,
     getCinemasByCityId,
     getMovieById,
     getCinemaById,
+    getPosterLinkByMovieId,
     getActorsByMovieId,
-    getSessionsByMovieIdAndDate
+    getSessionsByCityIdAndMovieIdAndDate,
+    getAllSessions,
+    getSessionInCinema
 };
 
 export default api
