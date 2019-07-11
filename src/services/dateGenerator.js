cc40c765b9c092f2a5c8470595d0639871b710e1
ostepano <ostepano@softserveinc.com>
2019-07-11 12:08:18 +0300

    date generator service fix

--- src/services/dateGenerator.js
+++ src/services/dateGenerator.js
@@ -1,8 +1,8 @@
 export const getCurrentDate = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    let month = date.getMonth();
-    let day = date.getDay();
+    const newDate = new Date();
+    const year = newDate.getFullYear();
+    let month = newDate.getMonth() + 1;
+    let day = newDate.getDate();
 
     if (month < 9) { month = "0" + month }
     if (day < 9) { day = "0" + day }
