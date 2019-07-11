29c8937b56f5a75dcdf490b0b9e3d66c14d5eaaa
ostepano <ostepano@softserveinc.com>
2019-07-11 16:32:41 +0300

    reducer update

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,38 @@
 const rootReducer = (state = {}, action) => {
-    console.log(state, action);
+    switch (action.type) {
+        case "UPDATE_CITY":
+            return {
+                ...state,
+                cityId: action.payload
+            };
+        case "UPDATE_DATE":
+            return {
+                ...state,
+                date: action.payload
+            };
+        case "UPDATE_URL":
+            return {
+                ...state,
+                date: action.payload
+            };
+        case "UPDATE_SELECTED_MOVIE":
+            return {
+                ...state,
+                selectedMovieId: action.payload
+            };
+        case "UPDATE_SELECTED_CINEMA":
+            return {
+                ...state,
+                selectedCinemaId: action.payload
+            };
+        case "UPDATE_SHOPPING_CART":
+            return {
+                ...state,
+                shoppingCart: action.payload
+            };
+        default:
+            return state
+    }
 };
 
 export default rootReducer;
