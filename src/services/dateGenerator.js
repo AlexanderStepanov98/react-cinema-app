c2733e62160394b2876b5d2d22b674435b427900
ostepano <ostepano@softserveinc.com>
2019-07-15 11:09:15 +0300

    dateGenerator service improvements

--- src/services/dateGenerator.js
+++ src/services/dateGenerator.js
@@ -1,11 +1,5 @@
 export const getCurrentDate = () => {
     const newDate = new Date();
-    const year = newDate.getFullYear();
-    let month = newDate.getMonth() + 1;
-    let day = newDate.getDate();
 
-    if (month < 9) { month = "0" + month }
-    if (day < 9) { day = "0" + day }
-
-    return `${year}-${month}-${day}`
+    return newDate.toISOString().slice(0,10)
 };
